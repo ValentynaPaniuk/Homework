f2fdf9e0587b9247714ea5a89dba5d3a0efafdd5
Valentyna Paniuk <Valentyna.paniuk@gmail.com>
2019-07-14 14:41:17 +0300

    Homework

---
+++ Source04072019.cpp
@@ -0,0 +1,15 @@
+# include <iostream>;
+# include <ctime>
+using namespace std;
+int main()
+
+{
+	srand(unsigned(time(NULL)));
+	setlocale(LC_ALL, "ru");
+
+	cout << "" << endl;
+
+		system("pause");
+		return 0;
+
+}
