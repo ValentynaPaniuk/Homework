07c9dc9b21910cc21998493c4ff62d0905e834bf
Valentyna Paniuk <Valentyna.paniuk@gmail.com>
2019-10-03 01:01:51 +0300

    Homework

--- Homework20190919_Dynamic_matrix/Homework20190919_Dynamic_matrix.cpp
+++ Homework20190919_Dynamic_matrix/Homework20190919_Dynamic_matrix.cpp
@@ -1,233 +1,9 @@
 # include <iostream>
 # include <ctime>
+
 using namespace std;
 
-
-//2. Функція заповнення матриці
-void Fill_matrix(int &row1, int &col1, int **arr1)
-{
-	for (int i = 0; i < row1; i++)
-	{
-		for (int j = 0; j < col1; j++)
-		{
-			arr1[i][j] = rand() % 10 + 1;
-		}
-	}
-}
-
-//3. Функція виводу матриці
-void Print_matrix(int &row1, int &col1, int **arr1)
-{
-	for (int i = 0; i < row1; i++)
-	{
-		for (int j = 0; j < col1; j++)
-		{
-			cout << arr1[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << "*****************************************************************" << endl;
-}
-
-//4. Функція доповнення матриці новим рядком
-void AddLastRow(int **arr1, int &row1, int &col1)
-{
-	int k = 1;
-	int **arr2 = new int*[row1 + 1];
-
-	for (int i = 0; i < row1 + k; i++)
-	{
-		arr2[i] = new int[col1];
-	}
-
-	for (int i = 0; i < row1 + k; i++)
-	{
-		for (int j = 0; j < col1; j++)
-		{
-			if (i < row1)
-			{
-				arr2[i][j] = arr1[i][j];
-			}
-			else
-			{
-				arr2[i][j] = k;
-			}
-			cout << arr2[i][j] << "\t";
-		}
-		cout << endl;
-	}
-
-	for (int i = 0; i < row1; i++)
-	{
-		delete[] arr2[i];
-	}
-
-	delete[] arr2;
-}
-
-// 5. Функція видалення з матриці рядка за вказаним номером
-
-void DelRow(int **arr1, int &row1, int &col1)
-{
-	int index_del = 0;
-	int iterator = 0;
-	int **arr2 = new int *[row1 + 1];
-	//-----------------------------------------------
-	
-		cout << "Enter row's index element delete: ";
-		cin >> index_del;
-	
-
-	for (int i = 0; i < row1; i++)
-	{
-		for (int j = 0; j < col1; j++)
-		{
-			arr2[i] = new int[col1];
-		}
-	}
-
-	
-	for (int i = 0; i < row1-1; i++)
-	{
-		for (int j = 0; j < col1; j++)
-		{
-			arr2[i][j] = arr1[i][j];
-			if (i >= index_del) 
-			{
-				
-				arr2[i][j] = arr1[i + 1][j];
-			}
-	
-		}
-	}
-	
-	row1--;
-	for (int i = 0; i < row1; i++)
-	{
-		for (int j = 0; j < col1; j++)
-		{
-			cout << arr2[i][j] << "\t";
-		}
-		cout << endl;
-	}
-//Як вивести через функцію Print????????????????????????????????????????????
-
-	for (int i = 0; i < row1; i++)
-	{
-		delete[] arr2[i];
-	}
-	delete[] arr2;
-}
-//================================================================================================
-//6. Функція вставки нового рядка за вказаним номером
-void AddNewRow(int **arr1, int &row1, int &col1)
-{
-	int **arr2 = new int*[row1 + 1];
-	for (int i = 0; i < row1 + 1; i++)
-	{
-		arr2[i] = new int[col1];
-	}
-
-	int number;
-	cout << "Enter the number of row do You want to add: " << endl;
-	cin >> number;
-
-	for (int i = 0; i <= row1+1; i++)
-	{
-		for (int j = 0; j < col1; j++)
-		{
-			
-			if (i < number)
-			{
-				arr2[i][j] = arr1[i][j];
-			}
-			else if(i>number)
-			{
-				arr2[i][j] = arr1[i-1][j];
-				
-			}
-			else if (i==number)
-			{
-				arr2[i][j] = 0;
-			}
-			
-			cout << arr2[i][j] << "\t";
-			
-		}
-		
-		cout << endl;
-	}
-	
-	
-	/*for (int i = 0; i < row1; i++)
-	{
-		for (int j = 0; j < col1; j++)
-		{
-			cout << arr2[i][j] << "\t";
-		}
-		cout << endl;
-	}*/
-
-	
-
-	for (int i = 0; i < row1; i++)
-	{
-		delete[] arr2[i];
-	}
-
-	delete[] arr2;
-
-}
-
-// 7. Функція доповнення матриці новим стовпчиком
-// 8. Функція видалення з матриці стовпця за вказаним номером
-// 9. Функція вставки нового стовпця за вказаним номером
-
-int main()
-{
-
-	srand(unsigned(time(NULL)));
-
-
-	int col, row;
-	cout << "Enter matrix size: [row fierst] " << endl;
-	cin >> row >> col;
-	cout << "**************************************" << endl;
-	// Вказівник на вказівники
-	int **arr = new int *[row];
-	//Вказівник на новий рядок
-	for (int i = 0; i < row; i++) {
-		{
-			arr[i] = new int[col];
-		}
-	}
-	//Заповнили матрицю даними
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			arr[i][j] = rand() % 10 + 1;
-		}
-	}
-	//Вивели матрицю на екран
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	// Видалили вказівник на колонки
-
-	for (int i = 0; i < row; i++) {
-
-		delete[] arr[i];
-	}
-
-	// Видалили вказівник на вказівники
-
-	delete[] arr;
-	arr = nullptr;
-
-	//====================================================================================================================================================================================
-	/*C++
+/*C++
 Написати функції для роботи з динамічною матрицею:
 1. Функція створення динамічної матриці розміром row x col, де row та col - аргументи функції. Функція повертає адресу створеної матриці (або адреса матриці передається по ссилці).
 2. Функція заповнення матриці
@@ -239,9 +15,40 @@ int main()
 8. Функція видалення з матриці стовпця за вказаним номером
 9. Функція вставки нового стовпця за вказаним номером */
 
-
-/*1. Функція створення динамічної матриці розміром row x col, де row та col - аргументи функції. 
+// Чистка пам'яті
+void Clean(int **arr1, int &row1);
+/*1. Функція створення динамічної матриці розміром row x col, де row та col - аргументи функції.
 	Функція повертає адресу створеної матриці (або адреса матриці передається по ссилці).*/
+void Create_matrix(int **arr1, int &row1, int &col1);
+
+//2. Функція заповнення матриці
+void Fill_matrix(int **arr1, int &row1, int &col1);
+
+//3. Функція виводу матриці
+void Print_matrix(int **arr1, int &row1, int &col1);
+
+//4. Функція доповнення матриці новим рядком
+void AddLastRow(int **arr1, int &row1, int &col1);
+
+// 5. Функція видалення з матриці рядка за вказаним номером
+void DelRow(int **arr1, int &row1, int &col1);
+
+//6. Функція вставки нового рядка за вказаним номером
+void AddNewRow(int **arr1, int &row1, int &col1);
+
+// 7. Функція доповнення матриці новим стовпчиком
+void AddLastCol(int **&arr1, int &row1, int &col1);
+
+// 8. Функція видалення з матриці стовпця за вказаним номером
+void DelCol(int **&arr1, int &row1, int &col1);
+
+// 9. Функція вставки нового стовпця за вказаним номером
+void AddNewCol(int **&arr1, int &row1, int &col1);
+
+
+int main()
+{
+	srand(unsigned(time(NULL)));
 
 	int col1 = 0, row1 = 0;
 	cout << "Enter the number of rows of the matrix: " << endl;
@@ -251,34 +58,246 @@ int main()
 	cout << "---------------------------------------------------------------------------" << endl;
 
 	int **arr1 = new int *[row1];
-
-	for (int i = 0; i < row1; i++)
-	{
-		arr1[i] = new int[col1];
-	}
-
-
-	//========================================================================================
-	Fill_matrix(row1, col1, arr1);
-	Print_matrix(row1, col1, arr1);
-	AddLastRow(arr1, row1, col1);
-
-
-	DelRow(arr1, row1, col1);
-
-	AddNewRow(arr1, row1, col1);
-
 	
-	//====================================================================================================
+	//1.
+	Create_matrix(arr1, row1, col1);
+	//2.
+	Fill_matrix(arr1, row1, col1);
+	//3.
+	Print_matrix(arr1, row1, col1);
+	//4.
+	cout << "ADD LAST ROW: " << endl;
+	AddLastRow(arr1, row1, col1);
+	Print_matrix(arr1, row1, col1);
+	//5.
+	cout << "DELETE ROW: " << endl;
+	DelRow(arr1, row1, col1);
+	Print_matrix(arr1, row1, col1);
+	//6.
+	cout << "ADD NEW ROW: " << endl;
+	AddNewRow(arr1, row1, col1);
+	Print_matrix(arr1, row1, col1);
+	//7.
+	cout << "ADD LAST COLUMN: " << endl;
+	AddLastCol(arr1, row1, col1);
+	Print_matrix(arr1, row1, col1);
+	//8.
+	cout << "DELETE COLUMN: " << endl;
+	DelCol(arr1, row1, col1);
+	Print_matrix(arr1, row1, col1);
+	//9.
+	AddNewCol(arr1, row1, col1);
+	Print_matrix(arr1, row1, col1);
 
-	for (int i = 0; i < row1; i++)
-	{
-		delete[] arr1[i];
-	}
-	delete[] arr1;
+	Clean(arr1, row1);
 	arr1 = nullptr;
 
 
 	system("pause");
 	return 0;
+}
+//------------------------------------------------------------------------------
+// 1. Створення динамічної матриці
+void Create_matrix(int **arr1, int &row1, int &col1)
+{
+	for (int i = 0; i < row1; i++)
+	{
+		arr1[i] = new int[col1];
+	}
+}
+
+//Чистка пам'яті матриці
+void Clean(int **arr1, int &row1)
+{
+	for (int i = 0; i < row1; i++)
+	{
+		delete[] arr1[i];
+	}
+	delete[] arr1;
+}
+
+//2. Функція заповнення матриці
+void Fill_matrix(int **arr1, int &row1, int &col1)
+{
+	for (int i = 0; i < row1; i++)
+	{
+		for (int j = 0; j < col1; j++)
+		{
+			arr1[i][j] = rand() % 10 + 1;
+		}
+	}
+}
+
+//3. Функція виводу матриці
+void Print_matrix(int **arr1, int &row1, int &col1)
+{
+	cout << "*****************************************************" << endl;
+	for (int i = 0; i < row1; i++)
+	{
+		for (int j = 0; j < col1; j++)
+		{
+			cout << arr1[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << "******************************************************" << endl;
+}
+
+//4. Функція доповнення матриці новим рядком
+void AddLastRow(int **arr1, int &row1, int &col1)
+{
+	int size = row1 + 1;
+	int **arr2 = new int*[size];
+	Create_matrix(arr2, size, col1);
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < col1; j++)
+		{
+			if (i < row1)
+			{
+				arr2[i][j] = arr1[i][j];
+			}
+			else
+			{
+				arr2[i][j] = 0;
+			}
+		}
+
+	}
+
+	Clean(arr1, row1);
+	row1++;
+	arr1 = arr2;
+}
+
+// 5. Функція видалення з матриці рядка за вказаним номером
+void DelRow(int **arr1, int &row1, int &col1)
+{
+	int index_del = 0;
+	row1--;
+	cout << "Enter row's index element delete: ";
+	cin >> index_del;
+	for (int i = 0; i < row1; i++)
+	{
+		for (int j = 0; j < col1; j++)
+		{
+			if (i >= index_del)
+			{
+				arr1[i][j] = arr1[i + 1][j];
+			}
+		}
+	}
+}
+
+//6. Функція вставки нового рядка за вказаним номером
+void AddNewRow(int **arr1, int &row1, int &col1)
+{
+	int number = 0;
+	int size = row1 + 1;
+	int **arr2 = new int *[size];
+	Create_matrix(arr2, size, col1);
+	cout << "Enter the number of row do You want to add: " << endl;
+	cin >> number;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < col1; j++)
+		{
+			if (i == number)
+			{
+				arr2[i][j] = 0;
+			}
+			else if (i < number)
+			{
+				arr2[i][j] = arr1[i][j];
+			}
+			else if (i > number)
+			{
+				arr2[i][j] = arr1[i - 1][j];
+			}
+		}
+	}
+	Clean(arr1, row1);
+	row1++;
+	arr1 = arr2;
+}
+
+// 7. Функція доповнення матриці новим стовпчиком
+void AddLastCol(int **&arr1, int &row1, int &col1)
+{
+	int size = col1 + 1;
+	int **arr2 = new int*[size];
+	Create_matrix(arr2, row1, size);
+
+	for (int i = 0; i < row1; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (j < col1)
+			{
+				arr2[i][j] = arr1[i][j];
+			}
+			else
+			{
+				arr2[i][j] = 0;
+			}
+		}
+
+	}
+
+	Clean(arr1, row1);
+	col1++;
+	arr1 = arr2;
+
+}
+
+// 8. Функція видалення з матриці стовпця за вказаним номером
+void DelCol(int **&arr1, int &row1, int &col1)
+{
+	int index_del = 0;
+	col1--;
+	cout << "Enter column's index element delete: ";
+	cin >> index_del;
+	for (int i = 0; i < row1; i++)
+	{
+		for (int j = 0; j < col1; j++)
+		{
+			if (j >= index_del)
+			{
+				arr1[i][j] = arr1[i][j + 1];
+			}
+		}
+	}
+}
+
+// 9. Функція вставки нового стовпця за вказаним номером
+void AddNewCol(int **&arr1, int &row1, int &col1)
+{
+	int number = 0;
+	int size = col1 + 1;
+	int **arr2 = new int *[size];
+	Create_matrix(arr2, size, col1);
+	cout << "Enter the number of column do You want to add: " << endl;
+	cin >> number;
+	for (int i = 0; i < row1; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (j == number)
+			{
+				arr2[i][j] = 0;
+			}
+			else if (j < number)
+			{
+				arr2[i][j] = arr1[i][j];
+			}
+			else if (j > number)
+			{
+				arr2[i][j] = arr1[i][j - 1];
+			}
+		}
+	}
+	Clean(arr1, row1);
+	row1++;
+	arr1 = arr2;
 }
