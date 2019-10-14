6bba6744f675e648faf66dfeb8157bbf1284e24f
Valentyna Paniuk <Valentyna.paniuk@gmail.com>
2019-10-15 00:37:57 +0300

    Homework

--- Homework20190926_String_Password/Homework20190926_String.cpp
+++ Homework20190926_String_Password/Homework20190926_String.cpp
@@ -3,72 +3,99 @@
 
 using namespace std;
 
+
+/*1. Перевірка паролів. Уявіть, що ви розробляєте програмний пакет, 
+який вимагає від користувачів введення власних паролів. 
+Програмне забезпечення вимагає, щоб паролі користувачів відповідали наступним критеріям:
+а. Пароль повинен бути не менше шести символів.
+б. Пароль повинен містити принаймні одну велику і малу літери.
+c. Пароль повинен мати принаймні одну цифру.
+Напишіть програму, яка запитує пароль, а потім перевіряє, чи відповідає вона зазначеним критеріям. 
+Якщо це не так, програма повинна відображати повідомлення, яке повідомляє користувачеві чому*/
+
 int main()
 {
 	char password[255] = {};
 	int counter[4];
 	bool exit = false;
 
-	cout << "Enter your password: " << endl;
-	cin >> password;
+	// Цикл, що спрацьовує доти, поки не буде безпечний пароль
+	for (int j = 0; exit == false; j++)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			counter[i] = 0;
+		}
 
-	for (int i = 0; ; i++)
-	{
-		password[i] = _getch();
-		if (int(password[i]) == 13)
+		// Ввід паролю
+		cout << "Enter your password: " << endl;
+		for (int i = 0; ; i++)
 		{
-			break;
+			
+			password[i]=_getch(); //Змінній "password" буде присвоєно код натиснутої клавіші
+			if (int(password[i]) == 13) // Пароль вводиться поки не буде введено клавішу "Ентер"
+			{
+				break;
+			}
 		}
-	}
+		// Перевірка всіх умов завдання: 
+		
+		for (int i = 0; i < strlen(password); i++)
+		{
+			// Перевірка чи довжина більша 6
+			if (strlen(password) >= 6)
+			{
+				counter[0] = 1;
+			}
+			// Перевірка на маленькі літери
+			if (int(password[i]) >= 97 && int(password[i]) <= 122)
+			{
+				counter[1] = 2;
+			}
+			//Перевірка на великі літери
+			if (int(password[i]) >= 65 && int(password[i]) <= 90)
+			{
+				counter[2] = 3;
+			}
+			//Перевірка на наявеність чисел в паролі
+			if (int(password[i]) >= 48 && int(password[i]) <= 57)
+			{
+				counter[3] = 4;
+			}
+		}
 
-	for (int i = 0; i < strlen(password); i++)
-	{
-		if (strlen(password) >= 6)
+		// Причини не відповідності паролю
+		if (counter[0] == 0)
 		{
-			counter[0] = 1;
+			cout << "Your passwort must have SIX letters" << endl;
 		}
-		if (int(password[i]) >= 97 && int(password[i]) <= 122)
+		if (counter[1] == 0)
 		{
-			counter[1] = 2;
+			cout << "Your password must have LOWERCASE letter" << endl;
 		}
-		if (int(password[i]) >= 65 && int(password[i]) <= 90)
+		if (counter[2] == 0)
 		{
-			counter[2] = 3;
+			cout << "Your password must have CAPITAL letter" << endl;
 		}
-		if (int(password[i]) >= 48 && int(password[i]) <= 57)
+		if (counter[3] == 0)
 		{
-			counter[3] = 4;
+			cout << "Your password must have NUMBER" << endl;
 		}
-	}
-	if (counter[0] == 0)
-	{
-		cout << "Your passwort must have SIX letters" << endl;
-	}
-	if (counter[1] == 0)
-	{
-		cout << "Your password must have LOWERCASE letter" << endl;
-	}
-	if (counter[2] == 0)
-	{
-		cout << "Your password must have CAPITAL letter" << endl;
-	}
-	if (counter[3] == 0)
-	{
-		cout << "Your password must have NUMBER" << endl;
-	}
 
-	int suma_counter = 0;
-	for (int i = 0; i < 4; i++)
-	{
-		suma_counter += counter[i];
-		if (suma_counter == 10)
-		{
-			cout << "You have excellent password" << endl;
-			exit = true;
-		}
-	}
-	
 
+		// Перевірка надійності паролю
+		int suma_counter = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			suma_counter += counter[i];
+			if (suma_counter >= 10)
+			{
+				cout << "You have excellent password" << endl;
+				exit = true;
+			}
+		}
+
+	}
 	
 
 	system("pause");
